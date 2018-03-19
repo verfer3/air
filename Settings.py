44f4221e313e8e5251d2fdf594b4694916f3cd9d
VEDE3 <37521006+verfer3@users.noreply.github.com>
2018-03-19 03:35:48 +0200

    Create Settings.py

---
+++ Settings.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+BOT_NAME = "NEOCashAirdropBot"
+BOT_NICKNAME = "NEOCashAirdrop2Bot"
+BOT_TOKEN = "546691766:AAHina2q9jb8pIBJD3eKjPG4IECATBV_tqU"
+ADMIN = 1
+
+MESSAGES = {
+	"INTRODUCTION": """
+Hello %s! Thanks for joining NEOCash Airdrop
+To get the NEOCash you must follow our instructions
+
+1. Enter your ETH address 
+2. Join Channel : https://t.me/NEOCash
+3. Follow Twitter : https://twitter.com/NEOCashOfficial
+4. Get More Instantly NCH tokens: 
+http://neocash.club/distribution.html
+	""",
+	"START": "Invite your friends to join airdrop: https://t.me/"+BOT_NICKNAME+"?start=%s\nGet 5 NCH for every 1 user!\nYour points: %s\nInvited users: %s",
+	"REGISTRATION_COMPLETED": "Registration completed.",
+	"EARNED_POINTS": "User joined with your referral link.\nYou earned <b>5 NCH</b>.",
+	"SEND_ADDRESS": "Send your ETH address.",
+	"WRONG_ADDRESS": "Wrong address format, retry.",
+}
