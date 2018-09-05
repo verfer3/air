bee4f0124b2d193dd98bd6fb45c2612922fca14a
VEDE3 <37521006+verfer3@users.noreply.github.com>
2018-09-05 15:52:18 +0300

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -7,7 +7,7 @@ ADMIN = 1
 
 MESSAGES = {
 	"INTRODUCTION": """
-Hello %s! Thanks for joining Neon Exchange Airdrop
+Hello %s! Thanks for joining NEX Airdrop
 To get the NEX you must follow our instructions
 
 1. Enter your NEO address 
@@ -19,7 +19,7 @@ https://twitter.com/neonexchange
  http://bit.ly/Withdraw-NEX-Airdrop 
 
 """,
-	"START": "Invite your friends to join airdrop: https://t.me/"+BOT_NICKNAME+"?start=%s\nGet 1 NEX for every 1 friend!\nYour Bonus: 5 NEX\nYour points: %s\nYour Referral NEX: %s",
+	"START": "Invite your friends to join airdrop: https://t.me/"+BOT_NICKNAME+"?start=%s\nGet 1 NEX for every 1 friend!\nYour Bonus: 5 NEX\nYour Referral NEX: %s\nYour Invited Friends: %s",
 	"REGISTRATION_COMPLETED": "Registration completed.",
 	"EARNED_POINTS": "Friend joined with your referral link.\nYou earned <b>1 NEX</b>.",
 	"SEND_ADDRESS": "Send your NEO address.",
