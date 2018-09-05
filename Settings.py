5ab412cd09cc882eea8abf38e126a210f642ad0d
VEDE3 <37521006+verfer3@users.noreply.github.com>
2018-09-05 14:56:48 +0300

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -13,14 +13,13 @@ To get the NEOCash you must follow our instructions
 1. Enter your ETH address 
 2. Join Channel : https://t.me/NEOCash
 3. Follow Twitter : https://twitter.com/NEOCashOfficial
-4. Get More Instantly NCH tokens: 
->>> urlparse('http://www.google.com/').hostname.split('.')[1]
-'google'
+4. How to withdraw NEX?
+ "test www.telegra.ph/Neon-Exchange-Airdrop-Withdraw-NEX-NOW-09-05" 
 
 """,
-	"START": "Invite your friends to join airdrop: https://t.me/"+BOT_NICKNAME+"?start=%s\nGet 5 NCH for every 1 user!\nYour points: %s\nInvited users: %s",
+	"START": "Invite your friends to join airdrop: https://t.me/"+BOT_NICKNAME+"?start=%s\nGet 5 NEX for every 1 user!\nYour points: %s\nInvited users: %s",
 	"REGISTRATION_COMPLETED": "Registration completed.",
 	"EARNED_POINTS": "User joined with your referral link.\nYou earned <b>5 NCH</b>.",
-	"SEND_ADDRESS": "Send your ETH address.",
+	"SEND_ADDRESS": "Send your NEO address.",
 	"WRONG_ADDRESS": "Wrong address format, retry.",
 }
